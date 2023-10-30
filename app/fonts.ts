7ac3778013799fc0cc550e67b4b1287a8c45a91d
abolfazl <souli.fsd@gmail.com>
2023-10-30 20:09:55 +0330

    init the project

---
+++ app/fonts.ts
@@ -0,0 +1,36 @@
+import { NextFontWithVariable } from 'next/dist/compiled/@next/font';
+import localFont from 'next/font/local';
+
+const poppins: NextFontWithVariable = localFont({
+  src: [
+    {
+      path: '../public/fonts/poppins/200-Poppins-ExtraLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/poppins/300-Poppins-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/poppins/400-Poppins-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/poppins/500-Poppins-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/poppins/600-Poppins-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-poppins',
+  display: 'swap',
+});
+
+export default poppins;
