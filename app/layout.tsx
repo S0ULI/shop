043e0a91d9a379b573d8c11286e5fc068ad59847
abolfazl <souli.fsd@gmail.com>
2023-10-30 23:36:04 +0330

    prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,20 @@
-import type { Metadata } from 'next'
-import poppins from './fonts'
-import './globals.css'
-
-
+import type { Metadata } from 'next';
+import poppins from './fonts';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'shop',
   description: 'clothing shop',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={poppins.className}>{children}</body>
     </html>
-  )
+  );
 }
